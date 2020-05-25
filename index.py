11f55d11f02d5a659bbdc6ca86a92f653aba051d
shel1977 <st-shel@yandex.ru>
2020-05-25 22:45:12 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,53 @@
+import tornado.ioloop
+import tornado.web
+import asyncio
+
+from sys import platform
+
+if platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())  # python-3.8.0a4
+
+
+class ServerData:
+    def __init__(self, data):
+        self.data = data
+
+
+state = ServerData('any data')
+
+class MainRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        response = {
+            'info': 'server ready on port 8888'
+        }
+        self.write(response)
+
+class DateRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        response = {
+            'data': state.data
+        }
+        self.write(response)
+
+
+class DateChangeHandler(tornado.web.RequestHandler):
+    def post(self):
+        json = tornado.escape.json_decode(self.request.body)
+        state.data = str(json["data"])
+        response = {
+            'info': 'data has been changed',
+            'data': state.data
+        }
+        self.write(response)
+
+
+if __name__ == "__main__":
+    app = tornado.web.Application([
+        (r"/", MainRequestHandler),
+        (r"/data", DateRequestHandler),
+        (r"/post", DateChangeHandler)
+    ])
+
+    app.listen(8888)
+    print("listening port 8888")
+    tornado.ioloop.IOLoop.instance().start()
